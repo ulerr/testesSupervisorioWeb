1ce24321398a67f8bef7403364830ba62d1747e3
Rafael Moreira <rafamuller05@gmail.com>
2025-07-21 17:24:50 -0300

    Início do projeto (template)

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import supernovaLogo from "./assets/supernova.png";
+import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <main className="container">
+      <h1>Teste pré-Alfa do supervisório web</h1>
+      <div className="row">
+        <a href="https://www.instagram.com/supernova.ufjf/" target="_blank">
+          <img src={supernovaLogo} className="logo supernova" alt="Supernova logo" />
+        </a>
+      </div>
+      <p>Clique no ícone da Supernova para acessar nosso Instagram.</p>
+
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+      <p>{greetMsg}</p>
+    </main>
+  );
+}
+
+export default App;
