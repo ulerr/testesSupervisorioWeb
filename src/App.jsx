bc609408b7999fb3a0db80fbe5c3e4c9e70162b4
Rafael Moreira <rafamuller05@gmail.com>
2025-07-22 11:50:15 -0300

    Remoção de elementos não utilizados

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import supernovaLogo from "./assets/supernova.png";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
